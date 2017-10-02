8d809fb00c2d3ea79bbefe2da626f0d06b8b1adb
Vasiliy Shubko <flit77@gmail.com>
2017-10-02 16:40:44 +0200

    zero downtime deploy

--- scaling/cluster.js
+++ scaling/cluster.js
@@ -12,6 +12,8 @@ const numberOfUsersInDB = function() {
 if (cluster.isMaster) {
   const cpus = os.cpus().length;
 
+  console.log(`Master PID: ${process.pid}`);
+
   console.log(`Forking for ${cpus} CPUs`);
   for (let i = 0; i < cpus; i++) {
     cluster.fork();
@@ -37,3 +39,25 @@ if (cluster.isMaster) {
 } else {
   require('./server');
 }
+
+process.on('SIGUSR2', () => {
+  const workers = Object.values(cluster.workers);
+
+  const restartWorker = workerIndex => {
+    const worker = workers[workerIndex];
+    if (!worker) return;
+
+    worker.on('exit', () => {
+      if (!worker.exitedAfterDisconnect) return;
+      console.log(`Exited process ${worker.process.pid}`);
+
+      cluster.fork().on('listening', () => {
+        restartWorker(workerIndex + 1);
+      });
+    });
+
+    worker.disconnect();
+  };
+
+  restartWorker(0);
+});
