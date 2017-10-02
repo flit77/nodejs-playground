17bdda6c0444f5f5f557dc59fa70fe4e19174043
Vasiliy Shubko <flit77@gmail.com>
2017-10-02 16:01:34 +0200

    simulating fetching db for child processes

--- scaling/cluster.js
+++ scaling/cluster.js
@@ -1,6 +1,14 @@
 const cluster = require('cluster');
 const os = require('os');
 
+// **** Mock DB Call
+const numberOfUsersInDB = function() {
+  this.count = this.count || 2;
+  this.count = this.count * this.count;
+  return this.count;
+};
+// ****
+
 if (cluster.isMaster) {
   const cpus = os.cpus().length;
 
@@ -9,9 +17,15 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
-  Object.values(cluster.workers).forEach(worker => {
-    worker.send(`Hello Worker ${worker.id}`);
-  });
+  const updateWorkers = () => {
+    const usersCount = numberOfUsersInDB();
+    Object.values(cluster.workers).forEach(worker => {
+      worker.send({ usersCount });
+    });
+  };
+
+  updateWorkers();
+  setInterval(updateWorkers, 10000);
 } else {
   require('./server');
 }
--- scaling/server.js
+++ scaling/server.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const pid = process.pid;
+let usersCount;
 
 process.on('message', msg => {
-  console.log(`Message from master: ${msg}`);
+  usersCount = msg.usersCount;
 });
 
 http
   .createServer((req, res) => {
     for (let i = 0; i < 1e7; i++); // simulate CPU work
-    res.end(`Handled by process ${pid}`);
+    res.write(`Handled by process ${pid}\n`);
+    res.end(`Users: ${usersCount}`);
   })
   .listen(8080, () => {
     console.log(`Started process ${pid}`);
